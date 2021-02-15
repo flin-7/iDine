185c06e2d4bfdc20a665bb1da74abd60bf4a3d46
flin-7 <flin.7@icloud.com>
2021-02-14 21:37:59 -0800

    Setting up

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Felix Lin on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
